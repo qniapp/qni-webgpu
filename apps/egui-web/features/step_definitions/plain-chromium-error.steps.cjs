bd7f07af561af47f887d97adab148ab22c18c484
Yasuhito Takamiya <yasuhito@hey.com>
2026-04-20 13:14:33 +0900

    test: add egui-web plain chromium error bdd scenario

---
+++ apps/egui-web/features/step_definitions/plain-chromium-error.steps.cjs
@@ -0,0 +1,29 @@
+const assert = require('node:assert/strict')
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+const { PLAIN_BROWSER_MODE, openPageForMode } = require('../support/browser.cjs')
+const { openEguiApp, waitForAppReady, readEguiError } = require('../support/egui-helpers.cjs')
+
+Given('the egui web app is open in plain chromium', async function () {
+  const page = await openPageForMode(this, PLAIN_BROWSER_MODE)
+  await openEguiApp(page, this.baseUrl)
+})
+
+When('the plain chromium session finishes loading', async function () {
+  assert.ok(this.page, 'expected egui page to be open')
+  await waitForAppReady(this.page)
+})
+
+Then('a visible WebGPU error is shown', async function () {
+  assert.ok(this.page, 'expected egui page to be open')
+
+  await this.page.waitForFunction(() => Boolean(window.__eguiError), null, {
+    timeout: 20_000,
+  })
+
+  assert.notEqual(await readEguiError(this.page), null)
+
+  const errorLocator = this.page.locator('[data-testid="webgpu-error"]')
+  await errorLocator.waitFor({ state: 'visible' })
+  assert.match(await errorLocator.innerText(), /WebGPU/i)
+})
